82bf61bde611005a730d1d10735897f9eb7f1837
Tan <tan@Tans-MacBook-Pro.local>
2018-01-02 21:53:40 +0700

    Initial Commit

---
+++ DigitalClock/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  DigitalClock
+//
+//  Created by Tan on 1/2/18.
+//  Copyright © 2018 Ngoc Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
